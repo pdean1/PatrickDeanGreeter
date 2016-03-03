bf06ee270baacce92055a2aa5cf620138ef67c66
pdean1 <pdean1@my.westga.edu>
2016-03-02 20:54:49 -0500

    Completed implementing the assignments walkthrough requirements. Starting on other requirements specified in the assignment description.

--- app/src/androidTest/java/edu/westga/cs6242/patrickdeangreeter/MainActivityTests.java
+++ app/src/androidTest/java/edu/westga/cs6242/patrickdeangreeter/MainActivityTests.java
@@ -1,7 +1,38 @@
 package edu.westga.cs6242.patrickdeangreeter;
 
-/**
- * Created by Patrick on 3/2/2016.
- */
-public class MainActivityTests {
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTests() {
+        super(MainActivity.class);
+    }
+
+    public void testActivityExists() {
+        MainActivity a = getActivity();
+        assertNotNull(a);
+    }
+
+    public void testGreet() {
+        MainActivity a = getActivity();
+        final EditText nameEditText = (EditText) a.findViewById(R.id.greet_edit_text);
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                nameEditText.requestFocus();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("Patrick");
+        getInstrumentation().waitForIdleSync();
+        Button greetButton = (Button) a.findViewById(R.id.greet_button);
+        TouchUtils.clickView(this, greetButton);
+        TextView greetMessage =
+                (TextView) a.findViewById(R.id.message_text_view);
+        String actualText = greetMessage.getText().toString();
+        assertEquals("Hello, Patrick!", actualText);
+    }
 }
